0e844890be36ab5d8a57a473fe57c124c649f677
mickleon <mickleon64@gmail.com>
2025-03-15 17:53:35 +0400

    fib is working iterative

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 
-int fib(int n) {
-    if (n == 1) return 0;
-    if (n == 2) return 1;
-    return fib(n - 2) + fib(n - 1);
-}
-
 using namespace std;
+
+void fib(int n) {
+    if (n >= 1) cout << 0 << endl;
+    if (n >= 2) cout << 1 << endl;
+    if (n > 2) {
+        int v1 = 0, v2 = 1, v3;
+        for (int i = 3; i <= n; i++) {
+            v3 = v1 + v2;
+            cout << v3 << endl;
+            v1 = v2;
+            v2 = v3;
+        }
+    }
+}
 
 int main() {
     int n;
     cin >> n;
-    cout << fib(n) << endl;
+    fib(n);
 }
